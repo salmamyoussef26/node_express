5ce19f358a1854d8c9c773f9054081f9ca6765e9
salmamyoussef26 <salmamyoussef1999@gmail.com>
2024-05-13 21:36:22 +0300

    PART 01 end

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,12 @@ WORKDIR /app
 
 COPY package.json . 
 
+# else npm install --only=production
+#install only prod dependencies and skip installing any dev dependencies(nodemon).
 ARG NODE_ENV
 RUN if [ "${NODE_ENV}" = "development" ]; \
     then npm install; \
-    else npm install --only=production; \
+    else npm install --only=production; \   
     fi
 
 COPY . . 
--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
+const {MONGO_IP, MONGO_USER, MONGO_PASS, MONGO_PORT} = require("./config/config.js");
 
 const app = express()
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+// this function will try to connect with mongodb for 5 sec before starting the app.
+const connectWithRetry = () =>{
+    mongoose
+    .connect(mongoURL)
+    .then(() => console.log("successfully connected to db"))
+    .catch((e) => {
+        console.log(e)
+        setTimeout(connectWithRetry, 5000)
+    });
+
+} 
+
+connectWithRetry();
 
 app.get("/", (req, res) => {
     res.send("<h2> Hi There!!!! </h2>");
